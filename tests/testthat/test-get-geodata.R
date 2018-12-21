18b33efaea658122d271ca09a009df5cf9ee07f3
Sam Albers <sam.albers@gov.bc.ca>
2018-12-21 14:55:36 -0800

    test basic CQL statement

--- tests/testthat/test-get-geodata.R
+++ tests/testthat/test-get-geodata.R
@@ -5,3 +5,11 @@ test_that("bcdc_get_geodata returns an sf object for a valid id", {
   expect_is(bc_airports, "sf")
   expect_equal(attr(bc_airports, "sf_column"), "geometry")
 })
+
+test_that("bcdc_get_geodata accept CQL strings to refine data call",{
+  one_well <- bcdc_get_geodata("ground-water-wells",
+                               query = "OBSERVATION_WELL_NUMBER=108")
+  expect_is(one_well, "sf")
+  expect_equal(attr(one_well, "sf_column"), "geometry")
+  expect_equal(nrow(one_well), 1)
+})
