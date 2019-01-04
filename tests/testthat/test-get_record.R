22f72a179c33b330dc3f00986c618842c7130e2b
Andy Teucher <andy.teucher@gov.bc.ca>
2019-01-04 13:16:45 -0800

    Tests: bcdc_get_record is flexible. Closes #3 (along with 07cb9bd0)

---
+++ tests/testthat/test-get_record.R
@@ -0,0 +1,15 @@
+context("test-get_record")
+
+test_that("bcdc_get_record works with slug and full url", {
+  skip_if_not(curl::has_internet())
+  expect_is(ret1 <- bcdc_get_record("https://catalogue.data.gov.bc.ca/dataset/bc-airports"),
+           "bcdc_record")
+  expect_is(ret2 <- bcdc_get_record("bc-airports"),
+           "bcdc_record")
+  expect_is(ret3 <- bcdc_get_record("https://catalogue.data.gov.bc.ca/dataset/76b1b7a3-2112-4444-857a-afccf7b20da8"),
+           "bcdc_record")
+  expect_is(ret4 <- bcdc_get_record("76b1b7a3-2112-4444-857a-afccf7b20da8"),
+           "bcdc_record")
+  expect_equal(ret1$title, "BC Airports")
+  lapply(list(ret2, ret3, ret4), expect_equal, ret1)
+})
