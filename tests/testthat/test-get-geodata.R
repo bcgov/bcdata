dd683bea007c7956efb331d17d020bbe7c217e0d
Sam Albers <sam.albers@gov.bc.ca>
2019-01-25 09:54:45 -0800

    skipping tests if no internet

--- tests/testthat/test-get-geodata.R
+++ tests/testthat/test-get-geodata.R
@@ -1,12 +1,14 @@
 context("testing ability of bcdc_get_geodata to retrieve for bcdc")
 
 test_that("bcdc_get_geodata returns an sf object for a valid id", {
+  skip_if_net_down()
   bc_airports <- bcdc_get_geodata("bc-airports")
   expect_is(bc_airports, "sf")
   expect_equal(attr(bc_airports, "sf_column"), "geometry")
 })
 
 test_that("bcdc_get_geodata accept CQL strings to refine data call",{
+  skip_if_net_down()
   one_well <- bcdc_get_geodata("ground-water-wells",
                                query = "OBSERVATION_WELL_NUMBER=108")
   expect_is(one_well, "sf")
