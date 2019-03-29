9ffe66376f8cf8c8887c0b9354b729cce3ef59a9
Sam Albers <sam.albers@gov.bc.ca>
2019-03-29 09:53:23 -0700

    re-exporting the pipe

---
+++ R/utils-pipe.R
@@ -0,0 +1,11 @@
+#' Pipe operator
+#'
+#' See \code{dplyr::\link[dplyr]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
