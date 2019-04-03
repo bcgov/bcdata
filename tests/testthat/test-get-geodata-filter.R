4cc0f758f242a98f6090ef14904b94aaf6c28924
Sam Albers <sam.albers@gov.bc.ca>
2019-04-03 15:49:42 -0700

    re-organizing some of the tests to reflect the stages of query

--- tests/testthat/test-get-geodata-filter.R
+++ tests/testthat/test-get-geodata-filter.R
@@ -10,3 +10,14 @@ test_that("bcdc_get_geodata accepts R expressions to refine data call",{
   expect_equal(attr(one_well, "sf_column"), "geometry")
   expect_equal(nrow(one_well), 1)
 })
+
+
+test_that("bcdc_get_geodata accepts R expressions to refine data call",{
+  skip_if_net_down()
+  one_well <- bcdc_get_geodata("ground-water-wells") %>%
+    filter(OBSERVATION_WELL_NUMBER == 108) %>%
+    collect()
+  expect_is(one_well, "sf")
+  expect_equal(attr(one_well, "sf_column"), "geometry")
+  expect_equal(nrow(one_well), 1)
+})
