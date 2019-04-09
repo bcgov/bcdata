41082388c44dd8849ea6934853e1564f5c8fb6ed
Sam Albers <sam.albers@gov.bc.ca>
2019-04-09 10:15:06 -0700

    add test for remote col name issue

--- tests/testthat/test-get-geodata-filter.R
+++ tests/testthat/test-get-geodata-filter.R
@@ -1,5 +1,14 @@
 context("testing ability of filter methods to narrow a wfs query")
 
+test_that("bcdc_get_geodata accepts R expressions to refine data call",{
+  skip_if_net_down()
+  one_well <- bcdc_get_geodata("ground-water-wells") %>%
+    filter(OBSERVATION_WELL_NUMBER == 108) %>%
+    collect()
+  expect_is(one_well, "sf")
+  expect_equal(attr(one_well, "sf_column"), "geometry")
+  expect_equal(nrow(one_well), 1)
+})
 
 test_that("bcdc_get_geodata accepts R expressions to refine data call",{
   skip_if_net_down()
@@ -11,23 +20,10 @@ test_that("bcdc_get_geodata accepts R expressions to refine data call",{
   expect_equal(nrow(one_well), 1)
 })
 
-
-test_that("bcdc_get_geodata accepts R expressions to refine data call",{
-  skip_if_net_down()
-  one_well <- bcdc_get_geodata("ground-water-wells") %>%
-    filter(OBSERVATION_WELL_NUMBER == 108) %>%
-    collect()
-  expect_is(one_well, "sf")
-  expect_equal(attr(one_well, "sf_column"), "geometry")
-  expect_equal(nrow(one_well), 1)
-})
-
-
-
-test_that("spatial operators work with SHAPE/GEOMETRY combinations",{
+test_that("operators work with different remote geom col names",{
   skip_if_net_down()
 
-  ## LOCAL "SHAPE"
+  ## LOCAL
   crd <- bcdc_get_geodata("regional-districts-legally-defined-administrative-areas-of-bc") %>%
     filter(ADMIN_AREA_NAME == "Cariboo Regional District") %>%
     collect()
@@ -39,7 +35,11 @@ test_that("spatial operators work with SHAPE/GEOMETRY combinations",{
   expect_is(em_program, "sf")
   expect_equal(attr(em_program, "sf_column"), "geometry")
 
-  ## Add each permutation of this
-
+  ## REMOTE "SHAPE"
+  crd_fires <- bcdc_get_geodata("fire-perimeters-historical") %>%
+    filter(FIRE_YEAR == 2000, FIRE_CAUSE == "Person", INTERSECTS(crd)) %>%
+    collect()
+  expect_is(crd_fires, "sf")
+  expect_equal(attr(crd_fires, "sf_column"), "geometry")
 
 })
