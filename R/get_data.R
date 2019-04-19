36e224041a2559fdd05af094c4b40e82dbaa8886
Sam Albers <sam.albers@gov.bc.ca>
2019-04-18 20:53:38 -0700

    adding xlsx option. refactoring bcdc_get_data code

--- R/get_data.R
+++ R/get_data.R
@@ -24,17 +24,18 @@
 #'
 #' @examples
 #' \dontrun{
-#' bcdc_get_data(paste0("https://catalogue.data.gov.bc.ca/dataset/",
-#'                      "british-columbia-greenhouse-gas-emissions/resource/",
-#'                      "11b1da01-fabc-406c-8b13-91e87f126dec"))
-#' bcdc_get_data("11b1da01-fabc-406c-8b13-91e87f126dec")
+#' bcdc_get_data("bc-airports")
+#' bcdc_get_data("bc-winery-locations", format = "csv")
+#' bcdc_get_data("local-government-population-and-household-projections-2018-2027",
+#' format = "xlsx", sheet = "Population", skip = 1)
+#'
 #' }
 bcdc_get_data <- function(x, format = NULL, ...) {
   UseMethod("bcdc_get_data")
 }
 
 #' @export
-bcdc_get_data.bcdc_record <- function(x) {
+bcdc_get_data.bcdc_record <- function(x, format = NULL, ...) {
   if(!has_internet()) stop("No access to internet", call. = FALSE)
 
   stop("not working yet!")
@@ -49,42 +50,39 @@ bcdc_get_data.bcdc_record <- function(x) {
 bcdc_get_data.character <- function(x, format = NULL, ...) {
   x <- slug_from_url(x)
 
-
   if(is.null(format)){
     query <- bcdc_query_geodata(x = x, ...)
-
     return(collect(query))
+  }
 
+  if(!all(format %in% c("csv","kml","txt","xlsx"))){
+    stop(paste0("The ", format, " extension is not currently supported by bcdata.
+                Try manually importing into R from this link: \n", file_url),
+         call. = FALSE)
   }
 
   record <- bcdc_get_record(x)
-
   record_formats <- purrr::map_chr(seq_along(record$resources),
                                    ~record$resources[[.x]][["format"]])
 
-  url <- record[["resources"]][[which(record_formats == format)]][["url"]]
+  file_url <- record[["resources"]][[which(record_formats == format)]][["url"]]
 
-  cli <- bcdc_http_client(url)
-
+  ## Do we need this?
+  cli <- bcdc_http_client(file_url)
   r <- cli$get()
   r$raise_for_status()
 
-
-  if(!all(format %in% c("csv","kml","txt"))){
-    stop(paste0("The ", ext, " extension is not currently supported by bcdata.
-        Try manually importing into R from this link: \n", file_url),
-        call. = FALSE)
-  }
-
+  ## Download file then read it in.
   tmp <- tempfile(fileext = paste0(".", format))
   on.exit(unlink(tmp))
-  utils::download.file(url, tmp, quiet = FALSE)
+  utils::download.file(file_url, tmp, mode = 'wb', quiet = FALSE)
 
   read_fun <- function(x, type) {
     switch(type,
            "csv" = readr::read_csv(x, ...),
            "kml" = bcdc_read_sf(x, ...),
-           "txt" = readr::read_tsv(x, ...))
+           "txt" = readr::read_tsv(x, ...),
+           "xlsx" = readxl::read_excel(x, ...))
   }
 
   read_fun(x = tmp, type = format)
