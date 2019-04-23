99cc12adf02cc0bae00bff3bdf5a14c55cf7daf3
Sam Albers <sam.albers@gov.bc.ca>
2019-04-23 10:04:08 -0700

    test with bcdc_get_data

--- tests/testthat/test-get-geodata.R
+++ tests/testthat/test-get-geodata.R
@@ -1,22 +1,22 @@
-context("testing ability of bcdc_get_geodata to retrieve an sf object")
+context("testing ability of bcdc_get_data to retrieve an sf object")
 
-test_that("bcdc_get_geodata collects an sf object for a valid id", {
+test_that("bcdc_get_data collects an sf object for a valid id", {
   skip_if_net_down()
-  bc_airports <- bcdc_get_geodata("bc-airports")
+  bc_airports <- bcdc_get_data("bc-airports")
   expect_is(bc_airports, "sf")
   expect_equal(attr(bc_airports, "sf_column"), "geometry")
 })
 
 
-test_that("bcdc_get_geodata works with slug and full url using collect", {
+test_that("bcdc_get_data works with slug and full url using collect", {
   skip_if_net_down()
-  expect_is(ret1 <- bcdc_get_geodata("https://catalogue.data.gov.bc.ca/dataset/bc-airports"),
+  expect_is(ret1 <- bcdc_get_data("https://catalogue.data.gov.bc.ca/dataset/bc-airports"),
             "sf")
-  expect_is(ret2 <- bcdc_get_geodata("bc-airports"),
+  expect_is(ret2 <- bcdc_get_data("bc-airports"),
             "sf")
-  expect_is(ret3 <- bcdc_get_geodata("https://catalogue.data.gov.bc.ca/dataset/76b1b7a3-2112-4444-857a-afccf7b20da8"),
+  expect_is(ret3 <- bcdc_get_data("https://catalogue.data.gov.bc.ca/dataset/76b1b7a3-2112-4444-857a-afccf7b20da8"),
             "sf")
-  expect_is(ret4 <- bcdc_get_geodata("76b1b7a3-2112-4444-857a-afccf7b20da8"),
+  expect_is(ret4 <- bcdc_get_data("76b1b7a3-2112-4444-857a-afccf7b20da8"),
             "sf")
   ## Must be a better way to test if these objects are equal
   expect_true(all(unlist(lapply(list(ret1, ret2, ret3, ret4), nrow))))
