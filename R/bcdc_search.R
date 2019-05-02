058cf6610fa395c37015d5871738c886612ea2ca
Andy Teucher <andy.teucher@gov.bc.ca>
2019-05-02 15:22:25 -0700

    Typos

--- R/bcdc_search.R
+++ R/bcdc_search.R
@@ -205,7 +205,7 @@ bcdc_get_record <- function(id) {
     get_record_warn_once(
       "It is advised to use the permanent id ('", ret$id, "') ",
       "rather than the name of the record ('", id,
-      "')to guard against future changes"
+      "') to guard against future name changes"
     )
   }
 
