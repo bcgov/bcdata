88993ecdfc3300c6a8599829039ff5a56f2ccb1b
Andy Teucher <andy.teucher@gov.bc.ca>
2019-06-12 16:25:00 -0700

    Consolidate bcdc_query_geodata method tests

--- tests/testthat/test-query-geodata.R
+++ tests/testthat/test-query-geodata.R
@@ -1,16 +1,19 @@
 context("testing the output of bcdc_query_geodata")
 
-test_that("bcdc_query_geodata returns an bcdc_promise object  for a valid id", {
+test_that("bcdc_query_geodata returns an bcdc_promise object for a valid id OR bcdc_record", {
   skip_if_net_down()
+  # id (character)
   bc_airports <- bcdc_query_geodata("bc-airports")
   expect_is(bc_airports, "bcdc_promise")
-})
 
-test_that("bcdc_query_geodata works with a bcdc_record", {
-  skip_if_net_down()
+  # bcdc_record
   bc_airports_record <- bcdc_get_record("bc-airports")
-  bc_airports <- bcdc_query_geodata(bc_airports_record)
-  expect_is(bc_airports, "bcdc_promise")
+  bc_airports2 <- bcdc_query_geodata(bc_airports_record)
+  expect_equal(bc_airports, bc_airports2)
+
+  # neither character nor bcdc_record
+  expect_error(bcdc_query_geodata(1L),
+               "No bcdc_query_geodata method for an object of class integer")
 })
 
 test_that("bcdc_query_geodata returns an object with a query, a cli and the catalogue object",{
