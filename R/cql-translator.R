8fe02ec9d40c0c75a7ccda899399cb50946bb21d
Andy Teucher <andy.teucher@gov.bc.ca>
2019-12-12 13:09:35 -0800

    Import := from rlang, document and fix test

--- R/cql-translator.R
+++ R/cql-translator.R
@@ -10,6 +10,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+#' @importFrom rlang :=
+
 # Function to translate R code to CQL
 cql_translate <- function(...) {
   ## convert dots to list of quosures
