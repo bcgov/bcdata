7dd965e31c10f7bfbcec0d9e95ae7ff52a5c1a5a
Andy Teucher <andy.teucher@gov.bc.ca>
2019-12-13 14:42:06 -0800

    Experimenting for #159

--- scratch/multipoint.R
+++ scratch/multipoint.R
@@ -10,4 +10,28 @@ format_wkt("POINT (1164434 368738.7)")
 format_wkt("MULTIPOINT (1164434 368738.7, 1203024 412959, 1203025 412960)")
 format_wkt("MULTIPOINT (1164434 368738.7 20, 1203024 412959 50, 1203025 412960 100)")
 
-st_as_text(st_as_sfc("MULTIPOINT ((10 10), (20 20))"))
+sf::st_as_text(sf::st_as_sfc("MULTIPOINT ((1236285 463726.8), (1228264 463547.4))"))
+
+httr::GET("https://openmaps.gov.bc.ca/geo/pub/wfs",
+          query = list(
+            SERVICE = "WFS",
+            VERSION = "2.0.0",
+            REQUEST = "GetFeature",
+            outputFormat = "application/json",
+            typeNames = "WHSE_BASEMAPPING.GBA_LOCAL_REG_GREENSPACES_SP",
+            SRSNAME = "EPSG:3005",
+            CQL_FILTER = "(INTERSECTS(SHAPE, MULTIPOINT (1236285 463726.8, 1228264 463547.4)))"
+          ))
+
+foo <- httr::GET("https://openmaps.gov.bc.ca/geo/pub/wfs",
+          query = list(
+            SERVICE = "WFS",
+            VERSION = "2.0.0",
+            REQUEST = "GetFeature",
+            outputFormat = "application/json",
+            typeNames = "WHSE_BASEMAPPING.GBA_LOCAL_REG_GREENSPACES_SP",
+            SRSNAME = "EPSG:3005",
+            CQL_FILTER = "(INTERSECTS(SHAPE, MULTIPOINT ((1236285 463726.8), (1228264 463547.4))))"
+          ))
+
+httr::content(foo)
