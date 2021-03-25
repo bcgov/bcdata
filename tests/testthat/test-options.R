919333c705d7b2ab102475f1f6fd7bacf68b56fe
Sam Albers <sam.albers@gov.bc.ca>
2021-03-25 15:46:29 -0700

    test if download limit option can be changed

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -34,6 +34,12 @@ test_that("bcdata.single_download_limit", {
 
 })
 
+test_that("bcdata.single_download_limit can be changes",{
+  lt <- withr::local_options(list(bcdata.single_download_limit = 13),
+                       getOption("bcdata.single_download_limit"))
+  expect_equal(lt$bcdata.single_download_limit, 13)
+})
+
 test_that("bcdc_single_download_limit returns a number",{
   skip_on_cran()
   skip_if_net_down()
