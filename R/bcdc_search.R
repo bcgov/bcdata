7e4b187fb1cd4874a58caff7cbb76443cdac4caf
Andy Teucher <andy.teucher@gmail.com>
2023-03-13 22:26:28 -0700

    documentation for searching with groups
    Co-authored-by: Stephanie Hazlitt <stephanie.hazlitt@gov.bc.ca>

--- R/bcdc_search.R
+++ R/bcdc_search.R
@@ -139,6 +139,8 @@ bcdc_list <- function() {
 #'        (see `bcdc_search_facets("sector")`)
 #' @param organization government organization that manages the data
 #'        (see `bcdc_search_facets("organization")`)
+#' @param groups collections of datasets for a particular project or on a particular theme
+#'        (see `bcdc_search_facets("groups")`)
 #' @param n number of results to return. Default `100`
 #'
 #' @return A list containing the records that match the search
@@ -153,6 +155,10 @@ bcdc_list <- function() {
 #' try(
 #'   bcdc_search("regional district", res_format = "fgdb")
 #' )
+#'
+#' try(
+#'   bcdc_search("angling", groups = "bc-tourism")
+#' )
 #' }
 bcdc_search <- function(..., license_id = NULL,
                         download_audience = NULL,
@@ -162,7 +168,7 @@ bcdc_search <- function(..., license_id = NULL,
                         groups = NULL,
                         n = 100) {
 
-  if(!has_internet()) stop("No access to internet", call. = FALSE) # nocov
+  if (!has_internet()) stop("No access to internet", call. = FALSE) # nocov
 
   # TODO: allow terms to be passed as a vector, and allow use of | for OR
   terms <- process_search_terms(...)
